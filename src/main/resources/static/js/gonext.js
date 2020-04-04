ac0cda71beee26362666ba8983101e70b74b22d5
Jinwoo Kim <ypk02007@naver.com>
2020-04-04 17:40:08 +0900

    gonext

--- src/main/resources/static/js/gonext.js
+++ src/main/resources/static/js/gonext.js
@@ -1,7 +1,11 @@
 var canvas = document.getElementById("canvas");
+var infoCanvas = document.getElementById("info");
 var ctx = canvas.getContext("2d");
+var ctx2 = infoCanvas.getContext("2d");
 var imgs = [];
 const complete = 5;
+
+var drawInterval;
 var lightX = 540;
 var playerX = 230;
 var playerY = -80;
@@ -10,7 +14,6 @@ var stageInfo;
 
 function init(stage) {
 	stageInfo = stage;
-	console.log("stageinfo: " + stageInfo);
 	loadImg();
 }
 
@@ -19,7 +22,7 @@ function loadImg() {
 	imgs.push(getImg("img/sprite/subway_door_left.png", "49", "88"));
 	imgs.push(getImg("img/sprite/subway_door_right.png", "49", "88"));
 	imgs.push(getImg("img/sprite/tunnel_light.png", "75", "15"));
-	imgs.push(getImg("img/character/gamjeon_lying.png", "80", "80"));
+	imgs.push(getImg("img/character/gamjeon.png", "400", "160"));
 	
 	loadCheck();
 }
@@ -52,7 +55,7 @@ function loadCheck() {
 		}
 		if(count == complete) {
 			console.log("load complete");
-			setInterval(draw, 20);
+			drawInterval = setInterval(draw, 20);
 			break;
 		} else {
 			count = 0;
@@ -62,6 +65,7 @@ function loadCheck() {
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx2.clearRect(0, 0, infoCanvas.width, infoCanvas.height);
 	
 	tunnelLightMove();
 	ctx.drawImage(imgs[3].img, lightX, 140);
@@ -70,7 +74,13 @@ function draw() {
 	ctx.drawImage(imgs[0].img, 0, 0);
 	drawPlayer();
 	
+	drawInfo();
+	
 	counter++;
+	if(counter >= 140) {
+		clearInterval(drawInterval);
+		console.log("interval ended");
+	}
 }
 
 function tunnelLightMove() {
@@ -86,11 +96,35 @@ function drawPlayer() {
 		playerY += 10;
 	} else if(counter >= 60 && counter < 80) {
 		var pitWidth = (counter - 60) * 4;
-		ctx.clearRect(270 - pitWidth/2, playerY + 10, pitWidth, 60);
+		ctx.clearRect(270 - pitWidth/2, playerY + 10, pitWidth, 70);
 	} else if(counter >= 80) {
-		ctx.clearRect(230, playerY + 10, 80, 60);
+		ctx.clearRect(230, playerY + 10, 80, 70);
 	}
 	if(counter < 100) {
-		ctx.drawImage(imgs[4].img, playerX, playerY);
+		ctx.drawImage(imgs[4].img, 320, 0, 80, 80, playerX, playerY, 80, 80);
+	} else {
+		var fallDown = (counter - 100) * 10;
+		if(fallDown > 80) {
+			fallDown = 80;
+		}
+		ctx.drawImage(imgs[4].img, 320, 0, 80, 80 - fallDown, playerX, playerY + fallDown, 80, 80 - fallDown);
+	}
+}
+
+function drawInfo() {
+	if(counter == 50) {
+		stageInfo++;
+	}
+	
+	for(var i = 0; i < 5; i++) {
+		ctx2.fillStyle = (i == stageInfo-1) ? '#C3C3C3' : '#585858';
+		ctx2.fillRect(45 + i * 100, 50, 50, 20);
+	}
+	
+	if(counter >= 30 && counter < 50) {
+		var headMove = (counter - 30) * 5;
+		ctx2.drawImage(imgs[4].img, 240, 0, 80, 80, 30 + (stageInfo-1) * 100 + headMove, 0, 80, 80);
+	} else {
+		ctx2.drawImage(imgs[4].img, 80, 0, 80, 80, 30 + (stageInfo-1) * 100, 0, 80, 80);
 	}
 }
